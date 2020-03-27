363d696712175c089a769cbf6007621b42c3bfe3
Vinay Srinivasan <vinaysrinivasan94@gmail.com>
2020-03-27 15:33:12 -0700

    can successfully scrape all past ladph media releases for cases

--- scrape.R
+++ scrape.R
@@ -6,7 +6,7 @@ rm(list = ls())
 
 ## Package management
 if(!require(pacman)) install.packages("pacman")
-pacman::p_load(data.table, tidyverse, rvest, stringr)
+pacman::p_load(data.table, tidyverse, rvest, stringr, assertable)
 
 ## Functions
 
@@ -23,9 +23,9 @@ is_scrapable <- function(page.obj) {
 }
 
 ## NEVER SCRAPED
-never_scraped <- function(url, prev.scraped.df){
+never_scraped <- function(url, prev.scraped.urls){
   
-  unscraped <- !(url %in% prev.scraped.df$url)
+  unscraped <- !(url %in% prev.scraped.urls)
   message(sprintf("NEVER SCRAPED: %s", unscraped))
   
   return(unscraped)
@@ -52,9 +52,7 @@ releases_df[, date := as.Date(date, format = "%B %d, %Y")]
 ## Loop through and scrape pages
 
 ## Load in already scraped data, so we're only scraping new releases
-scraped_df <- fread("ladph_covid_cases_scraped.csv")
-
-scrape_dfs <- data.table()
+scraped_urls <- import_files(list.files("data", pattern = "scraped", full.names = T)) %>% .[, unique(url)]
 
 for (release in 1:nrow(releases_df)){
   
@@ -63,12 +61,22 @@ for (release in 1:nrow(releases_df)){
   
   page.obj <- read_html(release_row$links)
   
-  if(is_scrapable(page.obj) & never_scraped(release_row$links, scraped_df)){
+  if(is_scrapable(page.obj) & never_scraped(release_row$links, scraped_urls)){
     
     # Select chunk corresponding to reported cases
     cases <- html_nodes(page.obj, css = "ul") %>% html_text
     cases <- grep(cases, pattern = "Hollywood", value = T) %>% trimws
-    cases <- str_replace_all(cases, "(?<=\\d)( *?)(?=[[A-Z],\\-])", "SPLIT") %>% str_split(., "SPLIT") # To match spaces between city elements and make fixed width
+    
+    # Hacky fix for 3/16 report
+    if(release_row$prid == 2268) cases <- str_replace(cases, "1<   City", "1  Culver City")
+    
+    # Hacky fix for 3/16 and 3/18 reports
+    if(release_row$prid %in% c(2268,2272)){
+      cases <- str_replace_all(cases, "(?<=\\d)( *?)(?=[[A-Z]])", "SPLIT") %>% str_split(., "SPLIT") # To match spaces between city elements and make fixed width
+      
+    } else {
+      cases <- str_replace_all(cases, "(?<=\\d)( *?)(?=[[A-Z],\\-?])", "SPLIT") %>% str_split(., "SPLIT") # To match spaces between city elements and make fixed width
+    }
     
     city_case_splitter <- if (all(grepl("--", unlist(cases)))) "--" else if (all(grepl("\\\t", unlist(cases)))) "\\\t" else "--|\\\t"
     
@@ -82,7 +90,10 @@ for (release in 1:nrow(releases_df)){
     cases[, date_reported := release_row$date]
     cases[, url := release_row$links]
     
-    scrape_dfs <- rbind(scrape_dfs, cases)
+    write.csv(cases, sprintf("data/ladph_covid_cases_scraped_%s.csv", str_remove_all(release_row$date, "-")), row.names = F)
+    
+    ## Try not to get blacklisted
+    Sys.sleep(sample(10, 1))
     
   } else {
     
@@ -90,9 +101,21 @@ for (release in 1:nrow(releases_df)){
   }
 }
 
-## ALSO DAILY SCRAPE THE TABLE THAT'S BEEN UPDATED AS A CROSS-REFERENCE/MOVING FORWARD THIS IS EASIER
-live_table_url <- "http://publichealth.lacounty.gov/media/Coronavirus/locations.htm"
-live_table <- read_html(live_table_url)
-
-# Write to file
-write.csv(cases, "ladph_covid_cases_scraped.csv", row.names = F)
+## Report for 3/21 needs separate scrape because slightly different formatting
+if(never_scraped(releases_df[prid == 2275, links], scraped_urls)){
+  
+  page.obj <- read_html(release_row$links)
+  cases <- html_nodes(page.obj, css = "li") %>% html_text
+  cases <- cases[grep("Alhambra", cases):grep("Under Investigation", cases)]
+  cases <- str_extract(cases, "^.*[:digit:]+")
+  cases <- tstrsplit(cases, "\t", names = c("city", "n_cases")) %>% as.data.table
+  cases[, n_cases := str_remove_all(n_cases, "[^[:digit:]]")]
+  cases[, city := str_remove_all(city, "[^[:alpha:][:space:]]")]
+  cases[, names(cases) := lapply(.SD, trimws, which = "both"), .SDcols = names(cases)]
+  cases[, n_cases := as.integer(n_cases)]
+  cases[, date_reported := releases_df[prid == 2275, date]]
+  cases[, url := release_row[prid == 2275, links]]
+  
+  write.csv(cases, sprintf("data/ladph_covid_cases_scraped_%s.csv", str_remove_all(unique(cases$date_reported), "-")), row.names = F)
+  
+}
