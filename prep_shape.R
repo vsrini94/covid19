844809ace5cc5dd924a5b13b164e9a10c450adc9
patrick <patrick@pyliu-mac-air.local>
2020-03-28 21:41:57 -0700

    cleaned shapefile

--- prep_shape.R
+++ prep_shape.R
@@ -4,80 +4,136 @@
 
 rm(list = ls())
 if(!require(pacman)) install.packages("pacman")
-pacman::p_load(data.table, stringr, maptools, sf, pdftools, splitstackshape)
+pacman::p_load(data.table, rio, tidyverse, stringr, maptools, sf, sp, pdftools, splitstackshape, mapview)
 
 # Cases reported by scrape (http://publichealth.lacounty.gov/media/Coronavirus/locations.htm) are listed by 
 # City/Community within LA County. Cannot identify a shapefile with both city/community borders, so will use 
 # Hierarchy listed here: and shapefile from
 
-## Prep Location Hierarchy 
-data <- pdf_data("data/hierarchy/ncovid19_regions_communities.pdf")
-hf <- NULL # hierarchy-frame
-for (i in 1:length(data)) {
-  df <- data[[i]]
-  ## Clean pdf_data (from Stackoverflow post: https://stackoverflow.com/questions/60127375/using-the-pdf-data-function-from-the-pdftools-package-efficiently)
-  df <- df %>% 
-    mutate(x = round(x/3), y = round(y/3)) %>% #reduce resolution to minimise inconsistent coordinates
-    arrange(y, x) %>%                        #sort in reading order
-    mutate(group = cumsum(!lag(space, default = 0))) %>%  #identify text with spaces and paste
-    group_by(group) %>% 
-    summarise(x = first(x),
-              y = first(y),
-              text = paste(text, collapse = " ")) %>% 
-    group_by(y) %>% 
-    mutate(colno = row_number()) %>%         #add column numbers for table data 
-    ungroup() %>% 
-    select(text, colno, y) %>% 
-    pivot_wider(names_from = colno, values_from = text) 
-  
-  ## Clean further: looks good, but line issue when ID has multiple rows of subunits
-  df <- df %>% data.table
-  df[, y:=NULL]
-  df <- df[!1:2,]
-  names(df) <- c("id", "label", "subunit", "pop")
-  
-  ## Some rows are shifted over by two columns starting at the end of "Region: n) x"
-  df[grepl(") ", label), `:=` (pop = subunit, subunit=tstrsplit(label, ") ")[[2]], label=tstrsplit(label, ") ")[[1]])]
-  
-  ## Fix when either label or subunit split across multiple lines
-  df[grepl("--", id), `:=` (label = id, id = NA)]
-  df[is.na(pop) & is.na(subunit) & is.na(label), `:=` (subunit = id, id=NA)]
-  ## Concatenate
-  df <- fill(df, id) %>% data.table ## Fill in ID by last non-NA value
-  df[, label := paste(label[!is.na(label)], collapse=" "), by=id] # Concatenate by id
-  df[, subunit := paste(subunit[!is.na(subunit)], collapse=" "), by=id] # Concatenate across subunit
-  df <- df[!is.na(pop)] # Rows that were concatenated have pop = NA, drop these
-  
-  df[, label := tstrsplit(label, "--")[[1]]]
-
-  hf <- rbind(hf, df) %>% data.table
+str.cap <- function(x) {
+  substr(x, 1, 1) <- toupper(substr(x, 1, 1))
+  return(x)
 }
- 
-## City = Los Angeles if subunit label has Los Angeles, otherwise City = name
-hf[grepl("Los Angeles-", subunit), city := "Los Angeles"]
-hf[is.na(city), city := label]
 
-## Expand Units by delimiter ";"
-hf <- cSplit(hf, "subunit", ";", "long")
+#--Setup-----------------------------------------
 
-## Create new column for unit type to try and match shapefile, clean subunit name
-hf <- hf[grepl("Unincorporated-", subunit), `:=` (type = "unincorporated", subunit = gsub("Unincorporated-", "", subunit))]
-hf <- hf[grepl("Los Angeles-", subunit), `:=` (type = "segment-of-a-city", subunit = gsub("Los Angeles-", "", subunit))]
+## Load Location hierarchy
+lf <- fread("data/hierarchy/ncovid19_hierarchy_prepped.csv")
+
+## Load data
+df <- fread("data/prepped/ladph_covid_cases_prepped_20200327.csv")
+
+## Renames
+setnames(df, c("location_number", "region_number"), c("location_id", "region_id"))
+
+## Clean names
+df[grepl("Unincorporated -", location_label), `:=` (type = "unincorporated-area", location_label = gsub("Unincorporated - ", "", location_label))]
+df[grepl("Los Angeles -", location_label), `:=` (type = "segment-of-a-city", location_label = gsub("Los Angeles - ", "", location_label))]
+df[is.na(type), type := "standalone-city"]
+
+## Cap for merge
+df[, location_label := toupper(location_label)]
+
+## Drop "Under Investigation"
+df <- df[location_id!=9999]
+
+#--Combine LA City and County shapefiles----------
+
+##Lets get LA County Borders
+cty <- st_read("data/shp/l.a. county neighborhood (current).shp")  %>% data.table
+cty[, location_label := toupper(name)]
+cty <- cty[, .(location_label, type, geometry)]
+## Drop LA City within this, will plan to swap with other shapefile 
+cty <- cty[type!="segment-of-a-city", .(location_label, type, geometry)] ## Drop LA City
+cty <- cty %>% st_sf
+
+## Grab LA City Neighborhoods
+la <- st_read("/Users/patrick/Downloads/LACITY_COMMUNITIES/LACITY_COMMUNITIES.shp") %>% data.table
+la <- la[, `:=` (location_label=COMTY_NAME, type="segment-of-a-city")]
+la <- la[, .(location_label, type, geometry)]
+la <- la %>% st_sf
+la <- st_transform(la, crs=4269) ## Conver to lat/long projection
+la <- st_cast(la, "MULTIPOLYGON")
+
+## Combine these two back to a single shapefile
+shp <- rbind(cty, la)
+
+## Merge
+shp <- shp %>% data.table
+shp[, shp_id := .I]
+
+## Locations witout names
+shp <- shp[!is.na(location_label)]
+
+## Rename shapefile to Covid names
+shp[location_label=="ALONDRA PARK", location_label := "EL CAMINO VILLAGE"] ## Rename Alondra Park -> El Camino Village
+shp[location_label=="ANGELES CREST", location_label := "ANGELES NATIONAL FOREST"] 
+shp[location_label=="CASTAIC CANYONS", location_label := "CASTAIC"] 
+shp[location_label=="CHARTER OAK", location_label := "CHARTEROAK"]
+shp[location_label=="EAST SAN GABRIEL", location_label := "NORTHEAST SAN GABRIEL"]
+shp[location_label=="LOPEZ/KAGEL CANYONS", location_label := "KAGEL/LOPEZ CANYONS"]
+shp[location_label=="SOUTH SAN JOSE HILLS", location_label := "SAN JOSE HILLS"]
+shp[location_label=="UNINCORPORATED CATALINA ISLAND", location_label := "SANTA CATALINA ISLAND"]
+shp[location_label=="UNINCORPORATED SANTA MONICA MOUNTAINS", location_label := "SANTA MONICA MOUNTAINS"]
+shp[location_label=="VIEW PARK-WINDSOR HILLS", location_label := "VIEW PARK/WINDSOR HILLS"]
+shp[location_label == "WEST WHITTIER-LOS NIETOS", location_label := "WEST WHITTIER/LOS NIETOS"]
+
+## Merge to df
+df <- merge(df, shp, by=c("location_label", "type"), all=T)
+
+#--Merge------------------------------------------
+
+## Aggregate shapes within a region_id if > 1 non-missing shape for that region
+
+## Re-slot shapes into regions
+df[location_label %in% c("ATHENS", "WESTMONT"), `:=` (region_id = 75, region_label="Athens-Westmont")]
+df[shp_id == 161, `:=`(region_id = 144, region_label="San Fernando")] # San Fernando -> San Fernando
+df[shp_id == 138, `:=`(region_id = 137, region_label="Studio City")] # Studio City -> Studio City
+df[shp_id == 27,  `:=`(region_id = 13, region_label="Azusa") ] # Citrus -> Azusa
+df[shp_id == 39,  `:=`(region_id = 63, region_label="Compton")] # East Compton -> Compton
+df[shp_id == 51,  `:=`(region_id = 2, region_label="Castaic")] # Green Valley -> Castaic
+df[shp_id == 53,  `:=`(region_id = 2, region_label="Castaic")]  # Hasley Canyon -> Castaic 
+df[shp_id == 84,  `:=`(region_id = 14, region_label="Arcadia")] # Mayflower Village -> Arcadia
+df[shp_id == 89,  `:=`(region_id = 7, region_label="Lake Los Angeles")]  # Northeast Antelope Valley -> Lake Los Angeles
+df[shp_id == 90,  `:=`(region_id = 27, region_label="El Monte")] # North El Monte -> El Monte
+df[shp_id == 91,  `:=`(region_id = 5, region_label="Lancaster")] # Northwest Antelope Valley -> Lancaster
+df[shp_id == 92,  `:=`(region_id = 6, region_label="Palmdale")] # Northwest Palmdale -> Palmdale
+df[shp_id == 102,  `:=`(region_id = 40, region_label="Walnut")] # Ramona -> Walnut
+df[shp_id == 106,  `:=`(region_id = 2, region_label="Castaic")] # Ridge Route -> Castaic
+df[shp_id == 121,   `:=`(region_id = 41, region_label="Diamond Bar")] # South Diamond Bar -> Diamond Bar  
+df[shp_id == 132,   `:=`(region_id = 155, region_label="Pacific Palisades")] # Topanga -> Pacific Palisades
+df[shp_id == 134,   `:=`(region_id = 141, region_label="Tujunga")] # Tujunga Canyon -> Tujunga
+df[shp_id == 137,  `:=`(region_id = 4, region_label="Stevenson Ranch")] # Unincorp. Santa Susana Mountains -> Stevenson Ranch
+df[shp_id == 142,  `:=`(region_id = 152, region_label="Brentwood")] # Veterans Administration -> Brentwood
+df[shp_id == 144,  `:=`(region_id = 24, region_label="Covina")] # Vincent -> Covina 
+df[shp_id == 148,  `:=`(region_id = 63, region_label="Compton")] # West Compton -> Compton
+df[shp_id == 154,  `:=`(region_id = 19, region_label="San Dimas")] # West San Dimas -> San Dimas
+df[shp_id == 312,  `:=`(region_id = 115, region_label="Highland Park")] # Arroyo View Estates -> Highland Park
 
 
-#----------------
+df[region_id==23, region_id := 24] # Unincorporated Covina ->  Covina
 
-## Load shapefile
-shp <- readShapePoly("data/shp/l.a. county neighborhood (v6).shp")
+## Regions where I'm going to aggregate shapefiles
+df[, n := sum(!is.na(shp_id)), by=region_id]
+ids <- df[is.na(shp_id) & n >= 1]$region_id %>% unique
+## Save these somewhere
+export(df[region_id %in% ids, .(region_id, region_label, location_id, location_label)], "data/hierarchy/agg_ids.csv")
 
-## Drop Orange County
-shp <- subset(shp, county != "orange")
+## Loop through region ids and aggregate the shapes within
+for (id in ids) {
+  to_merge <- df[region_id==id & !is.na(shp_id)]$shp_id
+  region <- df[region_id==id]$region_label[[1]] %>% toupper
+  n <- sum(df[region_id==id]$n_cases, na.omit=T)
+  ## Create new aggregated shape
+  agg <- df[shp_id %in% to_merge] %>% st_sf %>% st_combine %>% data.table
+  names(agg) <- "geometry"
+  agg[, `:=` (n_cases=n, region_id = id, region_label = region, location_id = id, location_label = region, shp_id = to_merge[[1]], type=NA)]
+  ## Drop smaller units from main shapefile and rbind this back
+  df <- df[region_id != id | is.na(region_id)]
+  df <- rbind(df, agg, fill=T)
+}
 
-## Create temp frame to assess merge
-sf <- shp@data %>% data.table
-sf <- sf[, .(name, city, type)]
-sf[, map.id := .I]
-
-## Attempt to slot in shapefile in to the reporting hierarchy
-hf <- merge(hf, sf, by.x="subunit", by.y="name", all.x=T)
+## Save shapefile
+df <- df[, .(location_label, location_id, region_id, shp_id, geometry)]
+export(df, "data/shp/shp_clean.rds")
 
